047d1869d4aec30d32f569c433e96d0d872cf759
Andrew Pashinnik <tobirawork@gmail.com>
2019-07-19 22:26:22 +0300

    refactor(date)

--- date.go
+++ date.go
@@ -9,42 +9,84 @@ import (
 
 var locale, _ = time.LoadLocation("Local")
 
-type DouDate struct {
+var months = map[string]time.Month{
+	// English months
+	"january":   time.January,
+	"february":  time.February,
+	"match":     time.March,
+	"april":     time.April,
+	"may":       time.May,
+	"june":      time.June,
+	"july":      time.July,
+	"august":    time.August,
+	"september": time.September,
+	"october":   time.October,
+	"november":  time.November,
+	"december":  time.December,
+	// Russian months
+	"января":   time.January,
+	"февраля":  time.February,
+	"марта":    time.March,
+	"апреля":   time.April,
+	"мая":      time.May,
+	"июня":     time.June,
+	"июля":     time.July,
+	"августа":  time.August,
+	"сентября": time.September,
+	"октября":  time.October,
+	"ноября":   time.November,
+	"декабря":  time.December,
+	// Ukrainian months
+	"січень":   time.January,
+	"лютий":    time.February,
+	"березень": time.March,
+	"квітень":  time.April,
+	"травень":  time.May,
+	"червень":  time.June,
+	"липень":   time.July,
+	"серпень":  time.August,
+	"вересень": time.September,
+	"жовтень":  time.October,
+	"листопад": time.November,
+	"грудень":  time.December,
+}
+
+type douDate struct {
 	day   int
 	month time.Month
 	year  int
 }
 
-func (dd DouDate) toTimeDate() time.Time {
-	if dd.isYearDefined() && dd.isMonthDefined() && dd.isDayDefined() {
+func (dd douDate) toTimeDate() time.Time {
+	if !dd.yearDefined() && !dd.monthDefined() && !dd.dayDefined() {
 		return time.Time{}
 	}
 
 	return time.Date(dd.year, dd.month, dd.day, 0, 0, 0, 0, locale)
 }
 
-func (dd DouDate) isMonthDefined() bool {
+func (dd douDate) monthDefined() bool {
 	return dd.month > 0
 }
 
-func (dd DouDate) isYearDefined() bool {
+func (dd douDate) yearDefined() bool {
 	return dd.year > 0
 }
 
-func (dd DouDate) isDayDefined() bool {
+func (dd douDate) dayDefined() bool {
 	return dd.day > 0
 }
 
-func newDouDateFromString(s string) DouDate {
-	re := regexp.MustCompile(`(?:\s*)(\d{1,2})(?:(?:\s+)([а-я]+))?(?:(?:\s+)(\d{4}))?`)
+func newDouDateFromString(s string) douDate {
+	var re = regexp.MustCompile(`(?:\s*)(\d{1,2})(?:(?:\s+)([а-яА-яa-zA-Z]+))?(?:(?:\s+)(?:(\d{4})|(?:\(.+\))))?`)
 
 	match := re.FindStringSubmatch(s)
 
-	date := DouDate{}
+	date := douDate{}
 
 	date.day, _ = strconv.Atoi(match[1])
 	if len(match[2]) != 0 {
-		date.month = getMonth(match[2])
+		date.month = defineMonth(match[2])
 	}
 	if len(match[3]) != 0 {
 		date.year, _ = strconv.Atoi(match[3])
@@ -53,64 +95,51 @@ func newDouDateFromString(s string) DouDate {
 	return date
 }
 
-func getMonth(m string) time.Month {
-	var months = map[string]time.Month{
-		"января":   time.January,
-		"февраля":  time.February,
-		"марта":    time.March,
-		"апреля":   time.April,
-		"мая":      time.May,
-		"июня":     time.June,
-		"июля":     time.July,
-		"августа":  time.August,
-		"сентября": time.September,
-		"октября":  time.October,
-		"ноября":   time.November,
-		"декабря":  time.December,
-	}
+func defineMonth(m string) time.Month {
+	m = strings.ToLower(m)
 
 	return months[m]
 }
 
-func getYearByMonth(month time.Month) int {
+func defineYearByMonth(month time.Month) int {
 	now := time.Now()
 
-	currentYear := now.Year()
-	currentMonth := now.Month()
-
-	if month < currentMonth {
-		return currentYear + 1
+	if now.Month() > month {
+		return now.Year() + 1
 	}
 
-	return currentYear
+	return now.Year()
 }
 
-func parseRawDate(rawDateString string) (time.Time, time.Time) {
-	var start DouDate
-	var end DouDate
+func parseRawDate(rawDate string) (time.Time, time.Time) {
+	var start douDate
+	var end douDate
 
-	rawDates := strings.Split(rawDateString, "—")
-	start = newDouDateFromString(rawDates[0])
+	dates := strings.Split(rawDate, "—")
 
-	hasEndDate := len(rawDates) == 2
+	start = newDouDateFromString(dates[0])
 
-	if hasEndDate {
-		end = newDouDateFromString(rawDates[1])
+	if len(dates) == 2 {
+		end = newDouDateFromString(dates[1])
 
-		if !end.isYearDefined() {
-			end.year = getYearByMonth(end.month)
+		if !end.yearDefined() {
+			end.year = defineYearByMonth(end.month)
 		}
 
-		if !start.isMonthDefined() {
+		if !start.monthDefined() {
 			start.month = end.month
 		}
 
-		if !start.isYearDefined() {
-			start.year = end.year
+		if !start.yearDefined() {
+			if start.monthDefined() {
+				start.year = defineYearByMonth(start.month)
+			} else {
+				start.year = end.year
+			}
 		}
 	} else {
-		if !start.isYearDefined() {
-			start.year = getYearByMonth(start.month)
+		if !start.yearDefined() {
+			start.year = defineYearByMonth(start.month)
 		}
 	}
 
